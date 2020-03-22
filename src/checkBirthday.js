70a32c9aa51d6cbd4b6a5dc107d59b2dee3dbc7e
Michael Burnley <about@michaelburnley.com>
2020-03-22 02:57:26 -0700

    moved try/catch

--- src/checkBirthday.js
+++ src/checkBirthday.js
@@ -5,16 +5,16 @@ import _ from 'lodash';
 import 'babel-polyfill';
 
 export default async (channel) => {
-    try {
-        const today = moment().format(`M/DD`);
-        
-        const current_birthdays = birthdays[today];
-        
-        for (const birthday of current_birthdays) {
+    const today = moment().format(`M/DD`);
+    
+    const current_birthdays = birthdays[today];
+    
+    for (const birthday of current_birthdays) {
+        try {
             const url = _.replace(`https://animalcrossing.fandom.com/wiki/${birthday}`, ` `, `_`);
-            channel.send(`It's ${birthday}'s birthday!\n\n${url}`)
+            channel.send(`It's ${birthday}'s birthday!\n\n${url}`);
+        } catch (err) {
+            console.log(err);
         }
-    } catch (err) {
-        console.log(err);
     }
 }
