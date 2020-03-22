975188830451f08e6bfb819994716e8feefbf449
Michael Burnley <about@michaelburnley.com>
2020-03-22 11:54:01 -0700

    making the message sending make more sense

--- src/checkBirthday.js
+++ src/checkBirthday.js
@@ -6,15 +6,13 @@ import 'babel-polyfill';
 
 export default (channel) => {
     const today = moment().format(`M/DD`);
-    
     const current_birthdays = birthdays[today];
-    
-    for (const birthday of current_birthdays) {
-        try {
-            const url = _.replace(`https://animalcrossing.fandom.com/wiki/${birthday}`, ` `, `_`);
-            channel.send(`It's ${birthday}'s birthday!\n\n${url}`);
-        } catch (err) {
-            console.log(err);
-        }
+    const urls = _.map(current_birthdays, birthday => (_.replace(`https://animalcrossing.fandom.com/wiki/${birthday}`, ` `, `_`)))
+    const names = _.join(current_birthdays, `'s and `);
+
+    try {
+        channel.send(`\:tada: It's **${names}**'s birthday! \:tada:\n\n${_.join(urls, `\n`)}`);
+    } catch (err) {
+        console.log(err);
     }
 }
